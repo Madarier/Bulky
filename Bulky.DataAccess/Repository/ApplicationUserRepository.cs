13d6dd05fcb040a35e9e6b1b5d2810fe27fdc098
Madarier <erkam-babayigit@hotmail.de>
2025-01-21 12:10:12 +0100

    added shopping cart and shopping cart ui

---
+++ Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.DataAccess.Data;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
